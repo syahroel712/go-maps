36acb76b080a015cb43c35669c69ce1de3f34f1f
syahroel712 <syahroel712@gmail.com>
2020-04-08 10:38:36 +0700

    #20 memperbaiki home aplikasi rute wisata

--- components/banner.php
+++ components/banner.php
@@ -2,8 +2,8 @@
     <div class="container h-100">
         <div class="row h-100 align-items-center">
             <div class="col-lg-12">
-                <h1 class="display-4 text-white mt-5 mb-2">Aplikasi Cari Rute Wisata</h1>
-                <p class="lead mb-5 text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore laborum itaque similique nostrum pariatur ad facilis soluta cumque nobis quo repudiandae odio aperiam molestias culpa rem debitis, error illum consequuntur.</p>
+                <h1 class="display-4 text-white mt-5 mb-2">Kabupaten Lima Puluh Kota</h1>
+                <p class="lead mb-5 text-white-50">Kabupaten Lima Puluh Kota (bahasa Minang: Limo Puluah Koto; Jawi, ليمو ڤولوه كوتو) adalah sebuah kabupaten di Provinsi Sumatra Barat, Indonesia. Ibu kota kabupaten ini terletak di Sarilamak. Kabupaten ini memiliki luas wilayah 3.354,30 km2 dan berpenduduk sebanyak 348.555 jiwa (Sensus Penduduk 2010)[2]. Kabupaten ini terletak di bagian timur wilayah provinsi Sumatra Barat atau 124 km dari Kota Padang, ibu kota provinsi. </p>
             </div>
         </div>
     </div>
